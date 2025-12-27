3c49b25f51249a0275439d35be4f8613f3b48eec
siddhant1729 <shaurya.sid1729@gmail.com>
2025-12-27 14:57:06 +0530

    added retrival refactor

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,15 @@
 from app.core.loaders import load_and_split_pdf
 from app.core.embeddings import get_embeddings
 from app.core.vectorstore import create_faiss_store
+from app.core.retriever import retrieve_from_Faiss
 
 def main():
     chunks = load_and_split_pdf("../data/raw/sample.pdf")
-    print(f"Total chunks: {len(chunks)}")
-
     embeddings = get_embeddings()
     vectorstore = create_faiss_store(chunks, embeddings)
 
     query = "What is the main issue discussed in the document?"
-    results = vectorstore.similarity_search(query, k=3)
+    results = retrieve_from_Faiss(vectorstore, query)
 
     for i, doc in enumerate(results, 1):
         print(f"\n--- Result {i} ---")
